423e22c9f618e977199ea8ccdb8f33712c8b6b6f
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-03-30 06:16:35 -0300

    JSX structure

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import { View, Text } from "react-native";
+
+export function Home() {
+  return (
+    <Fragment>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>React Native</Text>
+        <Text>Ignite</Text>
+      </View>
+      <Text>Ignite</Text>
+    </Fragment>
+  );
+}
