57e3d8dd6a70e5a86867b5f78cdd38139023ff4c
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-03-30 06:42:58 -0300

    Using StyleSheet in ReactNative

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,62 @@
-import React, { Fragment } from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export function Home() {
   return (
-    <Fragment>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>React Native</Text>
-        <Text>Ignite</Text>
-      </View>
-      <Text>Ignite</Text>
-    </Fragment>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Rodrigo</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>My Skills</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "#FFF",
+    fontSize: 18,
+    padding: Platform.OS === "ios" ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: "#a370f7",
+    padding: 15,
+    borderRadius: 7,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+});
