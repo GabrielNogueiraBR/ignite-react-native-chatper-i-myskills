c2525e7d5ca408d53a8fe568a87c111b9ef39c99
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-03-31 07:18:05 -0300

    Using useEffect with React Native

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -13,25 +13,37 @@ import { SkillCard } from "../components/SkillCard";
 export function Home() {
   const [newSkill, setNewSkill] = useState();
   const [mySkills, setMySkills] = useState([]);
+  const [gretting, setGretting] = useState("");
 
   function handleAddNewSkill() {
     setMySkills((oldState) => [...oldState, newSkill]);
   }
 
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+    if (currentHour < 12) setGretting("Good morning");
+    else if (currentHour >= 12 && currentHour <= 18) {
+      setGretting("Good afternoon");
+    } else {
+      setGretting("Good night");
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Rodrigo</Text>
+
+      <Text style={styles.greetings}>{gretting}</Text>
+
       <TextInput
         style={styles.input}
         placeholder="New skill"
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
       />
-
       <Button onPress={handleAddNewSkill} />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
-
       <FlatList
         data={mySkills}
         keyExtractor={(item, index) => index}
@@ -61,5 +73,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === "ios" ? 15 : 10,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greetings: {
+    color: "#fff",
   },
 });
