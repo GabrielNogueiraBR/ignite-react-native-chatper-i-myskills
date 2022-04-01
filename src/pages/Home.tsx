bbdb15c23aae4109bc835c6695ddceb92cff3195
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-04-01 10:20:52 -0300

    Adding functionality to remove Skill

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,6 +29,10 @@ export function Home() {
     setMySkills((oldState) => [...oldState, data]);
   }
 
+  function handleRemoveSkill(id: string) {
+    setMySkills((oldState) => oldState.filter((skill) => skill.id !== id));
+  }
+
   useEffect(() => {
     const currentHour = new Date().getHours();
     if (currentHour < 12) setGretting("Good morning");
@@ -57,7 +61,12 @@ export function Home() {
       <FlatList
         data={mySkills}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <SkillCard skill={item.name} />}
+        renderItem={({ item }) => (
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )}
       />
     </View>
   );
